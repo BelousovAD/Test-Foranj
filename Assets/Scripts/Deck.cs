9994aa1dd175e987b7388f1e18a7e662e42853bc
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-03-26 20:44:56 +0800

    Создан скрипт модели колоды

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class Deck : MonoBehaviour
+{
+    private const int MinIndex = -1;
+        
+    private readonly List<Card> _cards = new ();
+    private int _top = -1;
+
+    public bool IsFull => _top == _cards.Count - 1;
+
+    private void Awake() =>
+        _cards.AddRange(GetComponentsInChildren<Card>());
+
+    public void Push(FaceType face)
+    {
+        if (_top > MinIndex)
+        {
+            _cards[_top].Close();
+        }
+            
+        _cards[++_top].Initialize(face, true);
+    }
+
+    public void Pop()
+    {
+        if (_top > MinIndex)
+        {
+            _top--;
+        }
+    }
+}
